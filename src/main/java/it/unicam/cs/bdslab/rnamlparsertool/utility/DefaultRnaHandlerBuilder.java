16f09fbbcedc07e5e1d601b0236897b6e121f209
Piero <piero.hierro1@gmail.com>
2024-10-15 00:08:52 +0200

    Update DefaultRnaHandlerBuilder.java

--- src/main/java/it/unicam/cs/bdslab/rnamlparsertool/utility/DefaultRnaHandlerBuilder.java
+++ src/main/java/it/unicam/cs/bdslab/rnamlparsertool/utility/DefaultRnaHandlerBuilder.java
@@ -9,29 +9,30 @@ import it.unicam.cs.bdslab.rnamlparsertool.service.writer.NullFileWriter;
  * Implementazione di default per i casi non previsti.
  * Per aggiungere nuovi formati è necessario estendere diversamente
  * la classe astratta.
+ *
  * @author Marvin Sincini - Università di Informatica di Camerino - matricola 118311
  */
 public class DefaultRnaHandlerBuilder extends RnaHandlerBuilder {
 
-	@Override
-	protected RnaFileWriter buildUnexpectedFileWriter(String path) {
-		return new NullFileWriter();
-	}
+    @Override
+    protected RnaFileWriter buildUnexpectedFileWriter(String path) {
+        return new NullFileWriter();
+    }
 
-	@Override
-	protected RnaDataLoader buildUnexpectedDataLoader(String path) {
-		return new NullDataLoader();
-	}
+    @Override
+    protected RnaDataLoader buildUnexpectedDataLoader(String path) {
+        return new NullDataLoader();
+    }
 
-	@Override
-	public String[] getSupportedExtensions() {
-		return new String[]{"rnaml", "xml", "bpseq", "ct", "aas", "db" };
-	}
+    @Override
+    public String[] getSupportedExtensions() {
+        return new String[]{"rnaml", "xml", "bpseq", "ct", "aas", "db", "fasta"};
+    }
 
-	@Override
-	public String getDefaultExtension() {
-		return "xml";
-	}
-	
-	
+    @Override
+    public String getDefaultExtension() {
+        return "xml";
+    }
+
+
 }
